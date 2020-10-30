56935a9392d46c94449978af873cb49c3e5b7f26
vagrant <vagrant@vagrant.vm>
2020-10-30 20:02:38 +0000

    wrote for loop to populate database with movies - needs revision

--- seed_database.py
+++ seed_database.py
@@ -17,3 +17,20 @@ model.db.create_all()
 
 with open('data/movies.json') as f:
     movie_data = json.loads(f.read())
+
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+
+for movie in movie_data:
+    # TODO: get the title, overview, and poster_path from the movie
+    # dictionary. Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+    format = '%Y-%m-%d'
+    release_date = datetime.strptime(movie['release_date'], format)
+    genre = "horror"
+    db_movie = crud.create_movie(movie['title'], movie['overview'], movie['poster_path'], release_date, genre)
+    
+    # TODO: create a movie here and append it to movies_in_db
+    movies_in_db.append(db_movie)
